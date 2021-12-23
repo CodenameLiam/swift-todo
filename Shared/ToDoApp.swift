66226b9045177ac8911f1e67ea70a12c75cad116
Liam Percy <liampercy123@gmail.com>
2021-12-23 20:09:34 +1000

    initial commit

---
+++ Shared/ToDoApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoApp.swift
+//  Shared
+//
+//  Created by Liam Percy on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
